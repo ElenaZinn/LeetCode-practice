7ef8d346349e453184f9d38a6e825573ac9baa49
赵方晨 <zhaofangchen2018@bupt.edu.cn>
2022-01-15 18:13:08 +0800

    2022_01_15

--- 76._最小覆盖子串.java
+++ 76._最小覆盖子串.java
@@ -1,6 +1,6 @@
 // https://leetcode-cn.com/problems/minimum-window-substring/
 /**
-hint: shrinking window 
+hint: shrinking window，只有当valid==needs.size() && 当出现needs中包含的元素时,才从左侧缩小窗口，同时更新长度，移动窗口到出现needs中包含的元素停止
 test example 1
     s = "MABCEHIJKCABD" ,t = "ABE" , needs = {A:1,B:1,E:1}
     [MABCE],  windows = {A:1,B:1,E:1}, valid = 3 = needs.size(),start = left = 0, len = 6
@@ -9,10 +9,10 @@ test example 1
 
     [ABCEHIJKCA], windows = {A:1,B:1,E:1}, valid = 3, i-left>len
         d = B, valid = 2, windows = {A:1,B:0,E:1}
-    [ABCEHIJKCAB], windows = {A:1,B:1,E:1},  valid = 3,, i-left>len
-        d = C
+    [ABCEHIJKCAB], windows = {A:1,B:1,E:1},  valid = 3, i-left>len
+        d = C , i-left>len
         d = E,  valid = 2, windows = {A:1,B:1,E:0}
-    res = s.substring(start,start + len) = s.substring
+    res = s.substring(start, start + len) = s.substring(0, 4)
 
 test example 2
     s = "DBACKJIHECBAM", t = "ABE",  needs = {A:1,B:1,E:1}
@@ -22,13 +22,13 @@ test example 2
     [BACKJIHECB],  windows = {A:1,B:1,E:1}, valid = 3 = needs.size() 
         d = A, valid = 2, windows = {A:0,B:1,E:1} 
     [DBACKJIHECBA], windows = {A:1,B:1,E:1}, valid = 3 = needs.size() 
-        d = C
+        d = C, i-left>len
         d = K, start = 5, len = 7, [JIHECBA]
         d = J, start = 6, len = 6, [IHECBA]
         d = I, start = 7, len = 5, [HECBA]
         d = H, start = 8, len = 4, [ECBA]
         d = E, valid = 2, windows = {A:1,B:1,E:0}
-    [HECBAM]
+    res = s.substring(start, start + len) = s.substring(8,12)
 
 
  */
@@ -50,7 +50,7 @@ class Solution {
 
         
         i=0;
-        while (i<s.length()){
+        while (i<s.length()) {
             
             char c = s.charAt(i);
            
@@ -62,27 +62,19 @@ class Solution {
             
             }
             //update min length
-            while (valid == needs.size()){
-                System.out.println("i = " + i + " left = " + left + " len = " + len);
-                if (i-left + 1 <len){
+            while (valid == needs.size()) {
+                if (i-left + 1 <len) {
                     start = left;
                     len = i- left + 1;
-                    System.out.println("start = " + start + " s(start) = " + s.charAt(start)+ " s(left) = "+ s.charAt(left)+ " len = " + len);
-                    System.out.println("start =  " + start +  " len = " + len);
                 }
                 
                 char d = s.charAt(left ++ );
-                System.out.println("d = " + d);
-                if (windows.containsKey(d) ){
+
+                if (windows.containsKey(d) ) {
                     if (windows.get(d).equals(needs.get(d))) {
                         valid--;
-                         System.out.println("valid =  " + valid);
                     }
                     windows.put(d, windows.get(d) - 1);
-                    for (Character j : windows.keySet()) {
-            System.out.println("key: " + j+ " value: " + windows.get(j));
-        }
-        System.out.println("heyyyyy-------");
                 }
             }
 
