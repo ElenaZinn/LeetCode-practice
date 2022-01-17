c860dd04f54289ecd7668b8d2ae1f911e0bb60f5
赵方晨 <zhaofangchen2018@bupt.edu.cn>
2022-01-17 15:16:50 +0000

    2022_01_17

--- 3_无重复字符的最长子串_slidingWindow.java
+++ 3_无重复字符的最长子串_slidingWindow.java
@@ -1,21 +1,22 @@
 //https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
 /**
 example: s = "abcabcbb"
-right = 0, charFound = {a:1}, res = 1
-right = 1, charFound = {a:1,b:1}, res = 2
-right = 2,  charFound = {a:1,b:1,c :1}, res = 3
-right = 3,  charFound = {a:2,b:1,c :1}
-    s.charAt(left) = s.charAt(0) = a,charFound = {a:1,b:1,c :1} res = 3
-right = 4,  charFound = {a:1,b:2,c :1}
-     s.charAt(left) = s.charAt(1) = b,charFound = {a:1,b:1,c :1} res = 3
-right = 5,  charFound = {a:1,b:1,c :2}
-    s.charAt(left) = s.charAt(2) = c,charFound = {a:1,b:1,c :1} res = 3
-right = 6,  charFound = {a:1,b:2,c :1}
-    s.charAt(left) = s.charAt(3) = a,charFound = {a:0,b:2,c :1}
-    s.charAt(left) = s.charAt(4) = b,charFound = {a:0,b:1,c :1}
+right = 0, charFound = {a:1}, res = 1, elements in window: [a]
+right = 1, charFound = {a:1,b:1}, res = 2, [ab]
+right = 2,  charFound = {a:1,b:1,c :1}, res = 3, [abc]
+right = 3,  charFound = {a:2,b:1,c :1}, [abca]
+    s.charAt(left) = s.charAt(0) = a,charFound = {a:1,b:1,c :1} res = 3, [bca]
+right = 4,  charFound = {a:1,b:2,c :1}, [bcab]
+     s.charAt(left) = s.charAt(1) = b,charFound = {a:1,b:1,c :1} res = 3, [cab]
+right = 5,  charFound = {a:1,b:1,c :2}, [cabc]
+    s.charAt(left) = s.charAt(2) = c,charFound = {a:1,b:1,c :1} res = 3, [abc]
+right = 6,  charFound = {a:1,b:2,c :1}, [abcb]
+    s.charAt(left) = s.charAt(3) = a,charFound = {a:0,b:2,c :1}, [bcb]
+    s.charAt(left) = s.charAt(4) = b,charFound = {a:0,b:1,c :1}, [cb] 缩小窗口至不再有重复字母
     res = 3
-right = 7,  charFound = {a:0,b:2,c :1}
-    s.charAt(left) = s.charAt(5) = c,charFound = {a:0,b:1,c :1} res = 3
+right = 7,  charFound = {a:0,b:2,c :1}, [cbb]
+    s.charAt(left) = s.charAt(5) = c,charFound = {a:0,b:2,c :0} res = 3, [bb]
+    s.charAt(left) = s.charAt(6) = b,charFound = {a:0,b:1,c :0} res = 3,[b]
  */
 class Solution {
     //hint：sliding window
