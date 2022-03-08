66e7c7655d1be89c8055c7c68901164530a4e3a0
赵方晨 <zhaofangchen2018@bupt.edu.cn>
2022-03-08 23:15:00 +0800

    2022_03_08

---
+++ simple_101对称二叉树.java
@@ -0,0 +1,34 @@
+//https://leetcode-cn.com/problems/symmetric-tree/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        
+        return check(root.left, root.right);
+    }
+
+    boolean check(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left ==right;
+        }
+        if(left.val != right.val) {
+            return false;
+        }
+        //左右子节点需要对称相同
+        return check(left.right,right.left) && check(left.left, right.right);
+    }
+}
