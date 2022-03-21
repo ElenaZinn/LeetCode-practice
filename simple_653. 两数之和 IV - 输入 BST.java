c05e0589beed69a1dd30ab8939f2cebef562a53e
赵方晨 <zhaofangchen2018@bupt.edu.cn>
2022-03-21 22:39:43 +0800

    2022_03_21

---
+++ simple_653. 两数之和 IV - 输入 BST.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Set<Integer> res = new HashSet<>();
+    public boolean findTarget(TreeNode root, int k) {
+        //退栈条件
+        if(root==null) {
+            return false;
+        }
+        //对单个结点的操作
+        if(res.contains( k-root.val)) {
+            return true;
+        }
+        res.add(root.val);
+        //搜索以root.left || root.right为起点的满足要求的两个结点和
+        return findTarget(root.left, k) || findTarget(root.right,k);
+
+    }
+}
